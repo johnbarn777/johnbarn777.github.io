1f5f13497e6c0bedc30f5516be6e6e8f098c862a
johnbarn777 <johnbarn2409@gmail.com>
2025-07-15 22:37:45 -0700

    init new project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,5 @@
-import { defineConfig } from 'astro/config'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
-import compress from 'astro-compress'
-import icon from "astro-icon"
+// @ts-check
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-  site: 'https://johnbarn777.github.io/',
-  compressHTML: true,
-  integrations: [mdx(), icon(), tailwind({
-    applyBaseStyles: false,
-  }), compress()],
-  
-})
+export default defineConfig({});
